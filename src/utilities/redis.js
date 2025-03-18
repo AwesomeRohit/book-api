b3c35201621568eda6a03c6201704b935adf71f3
DissDude <44650354+AwesomeRohit@users.noreply.github.com>
2025-03-18 12:49:51 +0530

    updated port

--- src/utilities/redis.js
+++ src/utilities/redis.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv"
 dotenv.config();
 
 export const redis = new Redis({
-    host: 'redis-11449.crce179.ap-south-1-1.ec2.redns.redis-cloud.com',
-    port: 11449,
+    host: 'redis-11030.crce179.ap-south-1-1.ec2.redns.redis-cloud.com',
+    port: 11030,
     username: 'default',
     password: process.env.REDIS_PASSWORD,  // Use your actual password
   });
